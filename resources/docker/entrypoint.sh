a972d2c5d85a3dfab3cd2dffb3f248f1ed3782d8
Nicolas K <nkarageuzian@gmail.com>
2023-11-21 17:31:35 +0100

    Add explicit logging in entrypoint refs stalwartlabs/mail-server#149

--- resources/docker/entrypoint.sh
+++ resources/docker/entrypoint.sh
@@ -2,8 +2,22 @@
 # shellcheck shell=dash
 
 # If the configuration file does not exist wait until it does.
-while [ ! -f /opt/stalwart-mail/etc/config.toml ] || grep -q "__CERT_PATH__" /opt/stalwart-mail/etc/common/tls.toml; do
+
+loop_count=0
+while [ ! -f "${CONFIG}" ] || grep -q "__CERT_PATH__" /opt/stalwart-mail/etc/common/tls.toml; do
     sleep 1
+    if [ $(($loop_count % 30)) -eq 0 ]; then
+        if [ ! -f "${CONFIG}" ]; then
+            echo "ERROR: Configuration file ${CONFIG} not found."
+            echo "Please execute 'docker exec -it $HOSTNAME stalwart-install -p /opt/stalwart-mail -d' to fix and start service"
+        fi
+        if grep -q "__CERT_PATH__" /opt/stalwart-mail/etc/common/tls.toml; then
+            echo "ERROR: TLS not configured."
+            echo "Please check etc/common/tls.toml file in your configuration volume"
+        fi
+        echo "ERROR: Service will wait for configuration to be fixed before startup"
+    fi
+    loop_count=$(($loop_count+1))
 done
 
 # If the configuration file exists, start the server.
